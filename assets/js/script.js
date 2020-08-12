4c52cbe1dcd444dfb2eaa459603b17c56aa0dde6
Henry K <henrikas.kulikov@gmail.com>
2020-08-12 21:20:03 +0100

    Skeleton layout added for gen1 with loops for API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,66 @@
-$(document).ready(function () {
-    $("body").css("background-color", "blue");
-});
+/* jshint esversion: 9 */
+/* jshint -W069 */
+
+const pokedex = document.getElementById('pokedex');
+
+const fetchPokemon = async () => {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const pokemon = data.results.map((result, index) => ({
+        ...result, //object spread property
+        id: index + 1,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+    }));
+    displayPokemon(pokemon);
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map(
+        (pokeman) => `
+    <li class="card" onclick="selectPokemon(${pokeman.id})")>
+        <img class="card-image" src="${pokeman.image}"/>
+        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+    </li>
+    `).join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+
+/* pop-up / lightbox */
+
+const selectPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokeman = await res.json();
+    displayPopup(pokeman);
+};
+
+const displayPopup = (pokeman) => {
+    const type = pokeman.types.map(type => type.type.name).join(', ');
+    const image = pokeman.sprites['front_default'];
+    const htmlString = `
+    <div class="popup">
+        <button id="closeBtn" onclick="closePopup()">Close</button>
+        <div class="card">
+        <img class="popup-image" src="${image}"/>
+        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+        <p>
+        <small>Height: </small>${pokeman.height} | <small>Weight: </small>${pokeman.weight} | <small>Type: </small>${type} | <small>Abilities: </small>${pokeman.abilities.ability}
+        </p>
+        </div>
+    </div>
+    `;
+    pokedex.innerHTML = htmlString + pokedex.innerHTML;
+    console.log(htmlString);
+};
+
+const closePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.parentElement.removeChild(popup);
+};
+
+/* end of pop-up/ lightbox */
+
+fetchPokemon();
